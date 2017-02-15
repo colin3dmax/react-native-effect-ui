9263b4b0aa2f2d3f7225cae73af21bdc1f20063e
Cao Liang <colin3dmax@163.com>
2017-02-15 09:26:00 +0800

    upgrade to support react-native 0.41.2 remove @shoutem/u

--- android/app/src/main/java/com/everlist/MainApplication.java
+++ android/app/src/main/java/com/everlist/MainApplication.java
@@ -4,9 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import cl.json.RNSharePackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,10 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSharePackage(),
-            new LinearGradientPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
